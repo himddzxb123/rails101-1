cb43729e5cea27a8f42cb29fec8bc8343cd20e21
hizxb <himddzxb123@gmail.com>
2016-12-21 12:24:45 +0800

    implement group relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 has_many :groups
 has_many :posts
-has_many :group_ralationships
-has_many :participated_groups, :through => :group_ralationships, :source => :group
-     def is_member_of?(group)
-       participated_groups.include?(group)
-     end
+has_many :group_relationships
+has_many :participated_groups, :through => :group_relationships, :source => :group
 
 
 end
